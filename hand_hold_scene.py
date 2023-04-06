645cd534832864588510ff939e7d1c09577a0405
Mauricio Braz <aguiarbrazm@gmail.com>
2023-04-06 15:27:29 -0300

    Fine Tuning on methods

--- hand_hold_scene.py
+++ hand_hold_scene.py
@@ -1,5 +1,4 @@
 import obspython as obs
-import math, time
 import cv2
 import mediapipe as mp
 import numpy as np
@@ -15,8 +14,9 @@ def script_description():
             <p>Make fine adjust on Scale and Position Threshold (These adjusts are necessary due to imperfections on hand trackmotion in some video capture devices)</p>"""
 
 # Global variables to restore the scene item after shake
-move_sceneitem = None     # Reference to the modified scene item
-move_sceneitem_scale = obs.vec2()  # Initial rotation angle, used as well for oscillations
+move_sceneitem = None             # Reference to the modified scene item
+
+move_sceneitem_scale = obs.vec2() # Initial scale of the current on hold scene
 
 # Global variables to hotkeys usage
 hotkey_id_array = []
@@ -24,17 +24,17 @@ hotkey_names_by_id = {}
 open_camera = False
 
 # Global variables holding the values of data settings / properties
-source_name = "Image"  # Name of the source to shake
-move_scale_threshold = 10          # Moving Scale threshold to avoid animation oscillations from mediapipe detection
-move_pos_trheshold = 5             # Moving Position threshold to avoid animation oscillations from mediapipe detection
+source_name = "Image"     # Name of the source to shake
+move_scale_threshold = 10 # Moving Scale threshold to avoid animation oscillations from mediapipe detection
+move_pos_trheshold = 5    # Moving Position threshold to avoid animation oscillations from mediapipe detection
 debug = False
 
 # Data to smooth animation due to mediapipe jitter on detections
 smooth_data = {
   "pos_x": 0.5,
   "pos_y": 0.5,
-  "scale_x": 0.005,
-  "scale_y": 0.005
+  "scale_x": 0.0,
+  "scale_y": 0.0
 }
 
 #Basic Startup setting configuration to MediaPipe
@@ -52,10 +52,10 @@ cap = None
 
 # Saves the original rotation angle of the given sceneitem (assumed not None)
 def save_sceneitem_for_shake(sceneitem):
-  global move_sceneitem, move_sceneitem_scale
-  move_sceneitem = sceneitem
+  global move_sceneitem_scale, move_scene_handler
+
   obs.obs_sceneitem_get_scale(sceneitem, move_sceneitem_scale)
-  global move_scene_handler
+
   scene_as_source = obs.obs_scene_get_source(obs.obs_sceneitem_get_scene(sceneitem))
   move_scene_handler = obs.obs_source_get_signal_handler(scene_as_source)
   obs.signal_handler_connect(move_scene_handler, "item_remove", on_move_sceneitem_removed)
@@ -80,22 +80,22 @@ def get_sceneitem_from_source_name_in_current_scene(name):
 
 # Animates the scene item corresponding to source_name in the current scene
 def move_source():
-  sceneitem = get_sceneitem_from_source_name_in_current_scene(source_name)
-  if sceneitem:
-    font_id = obs.obs_sceneitem_get_id(sceneitem)
+  move_sceneitem = get_sceneitem_from_source_name_in_current_scene(source_name)
+  if move_sceneitem:
+    font_id = obs.obs_sceneitem_get_id(move_sceneitem)
     if move_sceneitem and obs.obs_sceneitem_get_id(move_sceneitem) != font_id:
       restore_sceneitem_after_move()
     if not move_sceneitem:
-      save_sceneitem_for_shake(sceneitem)
-    video_capture(sceneitem)
-    #angle = move_sceneitem_scale + move_pos_trheshold*math.sin(time.time()*move_scale_threshold*2*math.pi)
-    #obs.obs_sceneitem_set_rot(sceneitem, angle)
+      save_sceneitem_for_shake(move_sceneitem)
+    video_capture(move_sceneitem)
   else:
     restore_sceneitem_after_move()
 
 # Called every frame
 def script_tick(seconds):
   move_source()
+  #video_capture(move_sceneitem)
+
 
 
 def script_load(settings):
@@ -114,7 +114,6 @@ def script_load(settings):
 
         obs.obs_data_array_release(hotkey_data_array_from_settings)
   
-
 def hotkey_1_callback(is_pressed):
     # print(f"-- Shortcut 1 ; Data: {data}")
     if is_pressed:
@@ -140,18 +139,22 @@ move_scene_handler = None # Signal handler of the scene kept to restore
 
 # Called to set default values of data settings
 def script_defaults(settings):
-  global move_sceneitem_scale
-  move_sceneitem_scale = obs.vec2()
+  global move_sceneitem
   set_cap(cv2.VideoCapture(3))
   set_camera_open(True)
   switch_bool()
+  move_sceneitem = get_sceneitem_from_source_name_in_current_scene(source_name)
   obs.obs_data_set_default_string(settings, "source_name", "")
   obs.obs_data_set_default_double(settings, "move_scale_threshold", 10)
   obs.obs_data_set_default_int(settings, "move_pos_threshold", 5)
-  obs.obs_data_set_default_bool(settings, "debug", False)
+  obs.obs_data_set_default_bool(settings, "debug", False)  
+  obs.obs_sceneitem_get_scale(move_sceneitem, move_sceneitem_scale)
+  
+
 
 # Called to display the properties GUI
 def script_properties():
+  
   props = obs.obs_properties_create()
   # Drop-down list of sources
   list_property = obs.obs_properties_add_list(props, "source_name", "Source name",
@@ -169,15 +172,19 @@ def script_properties():
 
 # Called after change of settings including once after script load
 def script_update(settings):
-  global source_name, move_scale_threshold, move_pos_trheshold, debug
+  global source_name, move_scale_threshold, move_pos_trheshold, debug, move_sceneitem, move_sceneitem_scale
   restore_sceneitem_after_move()
   source_name = obs.obs_data_get_string(settings, "source_name")
   move_scale_threshold = obs.obs_data_get_double(settings, "move_scale_threshold")
   move_pos_trheshold = obs.obs_data_get_int(settings, "move_pos_threshold")
   debug = obs.obs_data_get_bool(settings, "debug")
+  move_sceneitem = get_sceneitem_from_source_name_in_current_scene(source_name)
+  obs.obs_sceneitem_get_scale(move_sceneitem, move_sceneitem_scale)
+  
 
 # Fills the given list property object with the names of all sources plus an empty one
 def populate_list_property_with_source_names(list_property):
+  
   sources = obs.obs_enum_sources()
   obs.obs_property_list_clear(list_property)
   obs.obs_property_list_add_string(list_property, "", "")
@@ -190,18 +197,19 @@ def video_capture(sceneitem):
   
     if(is_camera_open()):
 
-      try:
-  
-        success, image = get_cap().read()
-        if success:
-        # If loading a video, use 'break' instead of 'continue'.
-        # To improve performance, optionally mark the image as not writeable to
-        # pass by reference.
-          image.flags.writeable = False
-          image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-          results = get_hands().process(image)
+      success, image = get_cap().read()
+      if not success:
+        print ("Video Not Rendered")
+      # If loading a video, use 'break' instead of 'continue'.
+      # To improve performance, optionally mark the image as not writeable to
+      # pass by reference.
+      
+      else:
+        image.flags.writeable = False
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        results = get_hands().process(image)
 
-               
+        try:     
           for hand_landmarks in results.multi_hand_landmarks:             
       
             # Get coordinates
@@ -214,34 +222,31 @@ def video_capture(sceneitem):
                 tuple(np.multiply(point_index, [height, width]).astype(int)), 
                 cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
                 )
- 
+        except: #Exception as e: print(e.with_traceback)
+          pass
 
-          # Draw the hand annotations on the image.
-          image.flags.writeable = True
-          image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        # Draw the hand annotations on the image.
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-          # Render detections          
-          if results.multi_hand_landmarks:
-            obs.obs_sceneitem_set_visible(sceneitem, True)
-            for hand_landmarks in results.multi_hand_landmarks:
-              mp_drawing.draw_landmarks(
-                  image,
-                  hand_landmarks,
-                  mp_hands.HAND_CONNECTIONS,
-                  mp_drawing_styles.get_default_hand_landmarks_style(),
-                  mp_drawing_styles.get_default_hand_connections_style())
-          else:
-            obs.obs_sceneitem_set_visible(sceneitem, False)
+        # Render detections          
+        if results.multi_hand_landmarks:
+          obs.obs_sceneitem_set_visible(sceneitem, True)
+          for hand_landmarks in results.multi_hand_landmarks:
+            mp_drawing.draw_landmarks(
+                image,
+                hand_landmarks,
+                mp_hands.HAND_CONNECTIONS,
+                mp_drawing_styles.get_default_hand_landmarks_style(),
+                mp_drawing_styles.get_default_hand_connections_style())
+        else:
+          obs.obs_sceneitem_set_visible(sceneitem, False)
 
-          
-          cv2.imshow('Tracking Hands Debug', image)
+        #if debug:
+        cv2.imshow('Tracking Hands Debug', image)
 
-          del image
-          
-        
-      except:        
-        pass
-      
+        del image
+
     else:
       try:
         obs.obs_sceneitem_set_visible(sceneitem, False)
@@ -258,6 +263,8 @@ def switch_bool():
   
   if is_camera_open():
     set_camera_open(False)
+    #global move_sceneitem
+    #obs.obs_source_release(move_sceneitem)
     
   else:
     set_camera_open(True)
@@ -300,36 +307,30 @@ def calc_pos(results):
         float("{:.6f}".format(results[2]))
         ))
     , [multiply_coord[0],multiply_coord[1],multiply_coord[2]])
-  
   moving_source(coords)
 
   return tuple(coords).astype(int)
     
 def moving_source(coords):  
-    
-  sceneitem = get_sceneitem_from_source_name_in_current_scene(source_name)
-  
-  pos = obs.vec2()
-  scale = obs.vec()
+
+  global move_pos_trheshold, move_sceneitem, move_sceneitem_scale
+
+  local_pos = obs.vec2()
   local_scale = obs.vec2()
-  
-  obs.obs_sceneitem_get_scale(sceneitem, scale)
     
-  local_scale.x = (scale.x*0.1)* smooth_differ(coords[2], move_scale_threshold, "scale_x")
-  local_scale.y = (scale.y*0.1)* smooth_differ(coords[2], move_scale_threshold, "scale_y")
-  pos.x = smooth_differ(coords[0], move_pos_trheshold, "pos_x")
-  pos.y = smooth_differ(coords[1], move_pos_trheshold, "pos_y")
-  obs.obs_sceneitem_set_pos(sceneitem, pos)
-  obs.obs_sceneitem_set_scale(sceneitem, local_scale)
+  local_scale.x = (1+move_sceneitem_scale.x)* smooth_differ(coords[2], move_scale_threshold, "scale_x")*5
+  local_scale.y = (1+move_sceneitem_scale.y) * smooth_differ(coords[2], move_scale_threshold, "scale_y")*5
+  local_pos.x =   smooth_differ(coords[0], move_pos_trheshold, "pos_x")
+  local_pos.y =   smooth_differ(coords[1], move_pos_trheshold, "pos_y")
+  obs.obs_sceneitem_set_pos(move_sceneitem, local_pos)
+  obs.obs_sceneitem_set_scale(move_sceneitem, local_scale)
       
 def smooth_differ(value, percentage_cutoff, smooth_data_key):
-  
-  global smooth_data
-  
+    
+  global smooth_data  
   aux_coord = [smooth_data.get(smooth_data_key), value]
   
-  values_series = pd.Series(aux_coord)  
-  
+  values_series = pd.Series(aux_coord)    
   difference = abs((values_series.pct_change()[1])*100)
   
   if difference < percentage_cutoff:
